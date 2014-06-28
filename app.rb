9e975080a09f07186a5b7e7a6739f22d42b4d1f6
benevolent <mikio.fjt@gmail.com>
2014-06-28 10:31:23 +0900

    もう少しいいかんじにする

--- app.rb
+++ app.rb
@@ -1,21 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
-shop_list = [
-  "食神",
-  "大戸屋",
-  "そらまめ",
-  "らんちたいむ",
-  "学食",
-  "ハルモニア",
-  "西食堂",
-  "ラーメン二郎",
-  "マクドナルド",
-  "てんや",
-  "すた丼"
+wish_list = [
+  "画像投稿掲示板",
+  "Wiki",
+  "YouTubeもどき",
+  "適当なWebAPIを使ったツール",
+  "藤原竜也変換ツール",
+  "突然の死ジェネレーター（マンボウも可）",
+  "Twitterクライアント",
+  "mikutterプラグイン??",
+  "今まで作ったツールのデザインをいい感じにする",
+  "チャットアプリケーション(Rubyで)",
+  "らくがき投稿サイト",
+  "今まで作ったものを他の言語で書きなおしてみる",
+  "ライブラリを使ってミニゲーム"           
 ]
 
 get '/' do
-  shop_list.sample
+  "今回の開発は #{wish_list.sample} をつくりましょう"
 end
 
+get '/list' do
+  @list = wish_list
+  erb :list
+end
