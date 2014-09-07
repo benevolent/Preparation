6d1d46211e17920874ea1b12bb2d82764842aee2
benevolent <mikio.fjt@gmail.com>
2014-09-07 23:52:04 +0900

    /items以下に機能をまとめた

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require 'sinatra'
-require 'sinatra/reloader'
 require 'active_record'
+require 'sinatra/activerecord'
+require 'sinatra/reloader' if development?
 
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'sqlite3://localhost/enjoydevelopment.db')
 
 class List < ActiveRecord::Base
+  self.table_name = 'list'
 end
 
 helpers do
@@ -14,11 +16,9 @@ end
 
 get '/' do
   @title = 'たのしい開発練習'
-  @item  = List.all
-  #レコードが空の時に例外処理を行う.
+  @list  = List.all
   erb :index
 end
-
 
 get '/list' do
   @title = 'やりたいことリスト'
@@ -38,7 +38,7 @@ post '/delete' do
   redirect '/list'
 end
 
-
+=begin
 get '/:name' do
   @title = "#{params[:name]}"
   @item = List.find_by_title(params[:name])
@@ -46,4 +46,44 @@ get '/:name' do
     redirect '/'
   end
   erb :item
+end
+=end
+
+get '/items' do
+  @title = 'Item page'
+  @items = List.all
+  erb :'items/index'
+end
+
+get '/items/new' do
+  @title = 'Create new item page'
+  erb :'items/new'
+end
+
+get '/items/:id' do
+  @title = 'Show item page'
+  @item = List.find_by_id(params[:id])
+  erb :'items/show'
+end
+
+get '/items/:id/edit' do
+  @title = 'Update item page'
+  @item = List.find_by_id(params[:id])
+  erb :'items/edit'
+end
+
+post '/items/create' do
+  List.create({ :title => params[:title], :level => params[:level], :description => params[:description] })
+  redirect '/items'
+end
+
+post '/items/:id/update' do
+  @item = List.find_by_id(params[:id])
+  @item.update_attributes( :title => params[:title], :level => params[:level], :description => params[:description] )
+  redirect "/items/#{params[:id]}"
+end
+
+get '/items/:id/destroy' do
+  List.find_by_id(params[:id]).destroy
+  redirect '/items'
 end
